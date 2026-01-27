f56d0025cca720cdba5660b0a2aa6a3a14b073df
JbruceSecurity <jbruce@pwadc.com>
2026-01-27 05:49:39 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-export const SUPABASE_URL = "sb_publishable_U3CndPueW-qlj4qhsDgRnQ_06k0I1EH";
-export const SUPABASE_KEY = "sb_publishable_U3CndPueW-qlj4qhsDgRnQ_06k0I1EH";
+export const SUPABASE_URL = "https://uzjmpkesapbtykefnbnr.supabase.co";
+export const SUPABASE_KEY ="sb_publishable_U3CndPueW-qlj4qhsDgRnQ_06k0I1EH";
 export const APP_PIN = "2050";
