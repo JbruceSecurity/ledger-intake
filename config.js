83089ba97b90bfe928b6e50a35912227350aec57
JbruceSecurity <jbruce@pwadc.com>
2026-01-27 03:26:40 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 export const SUPABASE_URL = "https://zdwrjpaufxsbwgbhwwil.supabase.co";
-export const SUPABASE_KEY = "sb_publishable_U3CndPueW-qlj4qhsDgRnQ_06k0I1EH";
+export const SUPABASE_KEY = "sb_secret_ZR1uB0MN40oT7rza7qz3kw_6Wxj9jK4";
 export const APP_PIN = "2050";
